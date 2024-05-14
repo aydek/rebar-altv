fa7df70293d4ebbbfa4e60f999493a0becc7650c
aydek <kesa0915@hotmail.com>
2024-05-15 00:18:27 +0100

    Update config.ts

--- src/plugins/discord/server/config.ts
+++ src/plugins/discord/server/config.ts
@@ -1,10 +1,29 @@
+import { useConfig } from '@Server/config/index.js';
+
+declare global {
+    namespace NodeJS {
+        export interface ProcessEnv {
+            DISCORD_BOT_TOKEN: string;
+        }
+    }
+}
+
+declare module '@Server/config/index.js' {
+    interface Config {
+        // Name of key could be different from env variable, it doesn't matter.
+        discord_bot_token: string;
+    }
+}
+
+const config = useConfig();
+
+config.initFromEnv('discord_bot_token', {
+    env: 'DISCORD_BOT_TOKEN',
+    default: '',
+});
 
 export const DiscordConfig = {
-    BOT_TOKEN: "OTU1MjUwNzQ5ODY3MTc1OTY2.G5vrRe.5sP5gsmy_ZWBptFlxMk4BA5b0OSfKZI5MCQ4H8",
-    BOT_STATES: [
-        "[_player_count_/250] players connected",
-        "_vehicle_count_ vehicles spawned",
-        "sooo funny"
-    ],
+    BOT_TOKEN: config.getField('discord_bot_token'),
+    BOT_STATES: ['[_player_count_/250] players connected', '_vehicle_count_ vehicles spawned', 'sooo funny'],
     BOT_WEBSITE_URL: 'sabter.net',
-}
+};
