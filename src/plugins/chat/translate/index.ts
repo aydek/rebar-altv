fc988c9b2be70ecfced1d382f568bf20e84f8659
aydek <kesa0915@hotmail.com>
2024-10-22 10:06:09 +0100

    Create index.ts

---
+++ src/plugins/chat/translate/index.ts
@@ -0,0 +1,11 @@
+import { useTranslate } from '@Shared/translate.js';
+const { setBulk } = useTranslate();
+
+setBulk({
+    en: {
+        'chat:placeholder': 'Input message...',
+    },
+    lt: {
+        'chat:placeholder': 'Įveskite tekstą...',
+    },
+});
