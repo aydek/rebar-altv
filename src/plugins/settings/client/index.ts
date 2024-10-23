40d2451e8cf54348309e5e3a9f380086587e8664
aydek <kesa0915@hotmail.com>
2024-10-23 04:57:39 +0100

    Update index.ts

--- src/plugins/settings/client/index.ts
+++ src/plugins/settings/client/index.ts
@@ -1,52 +1,10 @@
-import * as alt from 'alt-client';
-import * as native from 'natives';
 import { useRebarClient } from '@Client/index.js';
-import { ISettings } from '../shared/types.js';
+
 import { SettingsEvents } from '../shared/events.js';
-import { clone } from '@Shared/utility/index.js';
-import { isNativeMenuOpen } from '@Client/menus/native/page.js';
-import { isWorldMenuOpen } from '@Client/menus/world/index.js';
 
 const Rebar = useRebarClient();
 const api = Rebar.useClientApi();
 const webview = Rebar.webview.useWebview();
-const messenger = Rebar.messenger.useMessenger();
-let isOpen = false;
-
-function getSettings() {
-    const chatSettings = api.get('chat-api');
-
-    const settings: ISettings[] = [
-        {
-            title: 'Chat',
-            icon: 'icon-chat',
-            options: [
-                {
-                    title: 'Timestamps',
-                    type: 'boolean',
-                    key: 'timestamps',
-                    value: chatSettings.get().timestamps,
-                },
-                {
-                    title: 'Automatically hide',
-                    type: 'boolean',
-                    key: 'autohide',
-                    value: chatSettings.get().autohide,
-                },
-                {
-                    title: 'Font size',
-                    type: 'slider',
-                    key: 'fontsize',
-                    value: chatSettings.get().fontsize,
-                    min: 0.7,
-                    max: 2,
-                },
-            ],
-        },
-    ];
-
-    webview.emit(SettingsEvents.toWebview.parse, clone.arrayData(settings));
-}
 
 function handleSettings(type: string, key: string, value: any) {
     switch (type) {
@@ -63,42 +21,3 @@ function handleSettings(type: string, key: string, value: any) {
 }
 
 webview.on(SettingsEvents.toClient.setSetting, handleSettings);
-
-alt.on('keyup', async (key: number) => {
-    if (webview.isAnyPageOpen()) {
-        return;
-    }
-
-    if (messenger.isChatFocused()) {
-        return;
-    }
-
-    if (isWorldMenuOpen()) {
-        return;
-    }
-
-    if (isNativeMenuOpen()) {
-        return;
-    }
-
-    if (alt.isConsoleOpen()) {
-        return;
-    }
-
-    if (key === 118) {
-        //f7
-        if (!isOpen) {
-            alt.toggleGameControls(false);
-            webview.show('Settings', 'page', true);
-            await alt.Utils.wait(50);
-            getSettings();
-        } else {
-            webview.hide('Settings');
-        }
-    }
-});
-
-webview.onClose('Settings', () => {
-    isOpen = false;
-    alt.toggleGameControls(true);
-});
