2a0fa50094ac4fb4e6fa5efd04b2f1a91ac0d929
aydek <kesa0915@hotmail.com>
2024-11-06 19:46:24 +0000

    Fixed minimap position

--- src/plugins/hud/client/index.ts
+++ src/plugins/hud/client/index.ts
@@ -1,5 +1,7 @@
-import { useRebarClient } from '@Client/index.js';
 import * as alt from 'alt-client';
+import * as native from 'natives';
+import { useRebarClient } from '@Client/index.js';
+
 import { HudEvents } from '../shared/events.js';
 import { HudSettingsKeys } from '../shared/settings.js';
 
@@ -18,6 +20,13 @@ webview.on(HudEvents.toClient.startInterval, () => {
     }
 
     interval = alt.setInterval(render, 50);
+
+    alt.setMinimapComponentPosition('minimap', 'L', 'B', -0.0045, -0.022, 0.15, 0.188888);
+    alt.setMinimapComponentPosition('minimap_mask', 'L', 'B', 0.02, 0.012, 0.111, 0.159);
+    alt.setMinimapComponentPosition('minimap_blur', 'L', 'B', -0.03, 0.002, 0.266, 0.237);
+
+    native.setBigmapActive(true, false);
+    native.setBigmapActive(false, false);
 });
 
 function render() {
