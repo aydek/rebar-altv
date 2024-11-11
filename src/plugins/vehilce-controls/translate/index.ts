7c13b2f7ab0e5c5f6ceff7780b0b32b0f3675382
aydek <kesa0915@hotmail.com>
2024-11-11 23:03:41 +0000

    Update index.ts

--- src/plugins/vehilce-controls/translate/index.ts
+++ src/plugins/vehilce-controls/translate/index.ts
@@ -3,12 +3,14 @@ const { setBulk } = useTranslate();
 
 setBulk({
     en: {
+        'vehiclecontrols:vehicle': 'Vehicle',
         'vehiclecontrols:engine': 'Engine',
         'vehiclecontrols:door': 'Door',
         'vehiclecontrols:bonnet': 'Bonnet',
         'vehiclecontrols:boot': 'Boot',
     },
     lt: {
+        'vehiclecontrols:vehicle': 'Transportas',
         'vehiclecontrols:engine': 'Variklis',
         'vehiclecontrols:door': 'Durys',
         'vehiclecontrols:bonnet': 'Kapotas',
