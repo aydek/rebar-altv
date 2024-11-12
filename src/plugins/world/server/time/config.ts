fa4ddb6fc819bac066121d2469e538d0357d0dfc
aydek <kesa0915@hotmail.com>
2024-11-12 00:10:32 +0000

    Time adjustment

--- src/plugins/world/server/time/config.ts
+++ src/plugins/world/server/time/config.ts
@@ -1,4 +1,7 @@
 export const TimeConfig = {
     useServerTime: false,
-    secondsPerSecond: 20,
+    secondsPerSecond: 5, // 1 hour will pass every 12 minutes
 };
+
+//3600÷5=720 real-life seconds
+//720÷60=12 minutes
