3acefdf2ccc70e43aea0d1b5c5e98cd9121bb084
aydek <kesa0915@hotmail.com>
2024-11-13 23:12:58 +0000

    Update index.ts

--- src/plugins/commands/server/index.ts
+++ src/plugins/commands/server/index.ts
@@ -1,10 +1,12 @@
 import * as alt from 'alt-server';
 import { useRebar } from '@Server/index.js';
 import { useNotificationAPI } from '@Plugins/notifications/server/api.js';
+import { useVitalityAPI } from '@Plugins/vitality/server/api.js';
 
 const Rebar = useRebar();
 const messenger = Rebar.messenger.useMessenger();
 const notifications = useNotificationAPI();
+const vitals = useVitalityAPI();
 
 messenger.commands.register({
     name: 'veh',
@@ -71,5 +73,49 @@ messenger.commands.register({
 
         player.armour = value;
         notifications.show(player, `Your armour set to: {00cc00}${value}`, 'success');
+    },
+});
+
+messenger.commands.register({
+    name: 'food',
+    desc: '[number] - Set your food',
+    callback: (player: alt.Player, val: string) => {
+        if (!player.valid) return;
+        const value = Number(val);
+
+        if (isNaN(value)) {
+            messenger.message.send(player, { content: '{cc0000} Invalid value', type: 'alert' });
+            return;
+        }
+
+        if (value > 100 || value < 0) {
+            messenger.message.send(player, { content: '{cc0000} Value has to be between 0 and 100', type: 'alert' });
+            return;
+        }
+
+        vitals.set(player, 'food', value);
+        notifications.show(player, `Your food set to: {00cc00}${value}`, 'success');
+    },
+});
+
+messenger.commands.register({
+    name: 'water',
+    desc: '[number] - Set your water',
+    callback: (player: alt.Player, val: string) => {
+        if (!player.valid) return;
+        const value = Number(val);
+
+        if (isNaN(value)) {
+            messenger.message.send(player, { content: '{cc0000} Invalid value', type: 'alert' });
+            return;
+        }
+
+        if (value > 100 || value < 0) {
+            messenger.message.send(player, { content: '{cc0000} Value has to be between 0 and 100', type: 'alert' });
+            return;
+        }
+
+        vitals.set(player, 'water', value);
+        notifications.show(player, `Your water set to: {00cc00}${value}`, 'success');
     },
 });
