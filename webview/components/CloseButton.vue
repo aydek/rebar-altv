944be1ded8043ac579c4046317d7c20a0b90d68c
aydek <kesa0915@hotmail.com>
2024-11-13 21:57:56 +0000

    Create CloseButton.vue

---
+++ webview/components/CloseButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Icon from './Icon.vue';
+import Button from './Button.vue';
+import { twMerge } from 'tailwind-merge';
+
+const props = defineProps<{
+    class?: string;
+}>();
+
+const emit = defineEmits<{
+    onClose: [];
+}>();
+
+const close = () => {
+    emit('onClose');
+};
+</script>
+
+<template>
+    <Button type="icon" :class="twMerge('absolute right-2 top-2 flex items-center justify-center gap-2', props.class && props.class)" @click="close">
+        <Icon icon="icon-close" />
+        <div>ESC</div>
+    </Button>
+</template>
